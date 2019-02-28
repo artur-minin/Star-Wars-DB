ccf0b2f336b6d2a16ac40d67c91729966fa47cd9
Artur <king.199@mail.ru>
2019-02-28 04:28:58 +0300

    added logic in random-planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,33 +1,85 @@
 import React, { Component } from 'react';
 
+import Preloader from '../preloader'
+import SwapiService from '../../services/swapi-service';
+
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+    loading: true
+  }
+
+  // Конструктор вызывается при создании класса, например при загрузке страницы
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  // Функция получает объект с необходимыми св-ми из swapi-service и перезаписывает state этими данными
+  onPlanetLoaded = (planet) => {
+    // В данном случае меняем state напрямую т.к. он не зависит от предыдущего состояния
+    this.setState({ planet, loading: false })
+  }
+
+  updatePlanet() {
+    const id = 3;
+
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded);
+  }
+
   render() {
+
+    const { planet, loading } = this.state;
+    
+    // Если контент еще не загружен, то отображается preloader иначе content
+    const preloader = loading ? <Preloader /> : null;
+    const content = !loading ? <PlanetView planet={planet} /> : null;
+
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-          src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
-        <div>
-          <h4>Planet Name</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>123124</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>43</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>100</span>
-            </li>
-          </ul>
-        </div>
+        { preloader }
+        { content }
       </div>
+    )
 
-    );
   }
+}
+
+// PlanetView отвечает за отображение данных и картинки планеты из блока random-planet
+const PlanetView = ({ planet }) => {
+
+  // Деструктурируем св-ва из объекта planet
+  const { id, name, population, rotationPeriod, diameter } = planet;
+
+  return (
+    // React.Fragment - это элемент-обертка, необходимый для группировки эл-ов, т.к. функция render может возвращать только 1 эл-т
+    <React.Fragment>
+      <img className="planet-image"
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diamater</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
 }
---
+++ src/services/swapi-service.js
@@ -0,0 +1,90 @@
+export default class SwapiService {
+
+  async getResource(url) {
+    const _baseUrl = 'https://swapi.co/api';
+
+    const res = await fetch(`${_baseUrl}${url}`);
+
+    // Если код ответа сервера не 200-299, то генерируем ошибку и она попадает в блок catch
+    if (!res.ok) {  
+      throw new Error(`Could not fetch ${url}, received ${res.status}.`);
+    }
+
+    // Если все OK, то возвращаем данные в виде JSON объекта
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results.map(this._transformPerson);
+  }
+
+  async getPerson(id) {
+    const person = await this.getResource(`/people/${id}/`);
+    return this._transformPerson(person);
+  }
+  
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results.map(this._transformPlanet);
+  }
+
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transfromPlanet(planet);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results.map(this._transformStarship);
+  }
+
+  async getStarship(id) {
+    const starship = await this.getResource(`/starships/${id}/`);
+    return this._transformStarship(starship);
+  }
+
+  // Функция "вытаскивает" id из поля url в JSON ответе от сервера
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  // Функция берет из JSON только необходимые нам поля для планет
+  _transfromPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    }
+  }
+
+  // Функция берет из JSON только необходимые нам поля для кораблей
+  _transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    }
+  }
+
+  // Функция берет из JSON только необходимые нам поля для людей
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    }
+  }
+
+}
