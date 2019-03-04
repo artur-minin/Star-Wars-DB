6490d446f24f09b1f4307fcf22487c267b039cf5
Artur <king.199@mail.ru>
2019-03-05 02:53:08 +0300

    added react-router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
+import './app.css';
+
 import { SwapiServiceProvider } from '../swapi-service-context';
 import SwapiService from '../../services/swapi-service';
 
 import ErrorBoundary from "../error-boundary";
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import {
-  PeoplePage,
-  StarshipsPage,
-  PlanetsPage
-} from '../pages';
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 
-import './app.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { PersonDetails, StarshipDetails, PlanetDetails } from '../sw-components';
 
 
 export default class App extends Component {
@@ -22,18 +21,36 @@ export default class App extends Component {
   render() {
     return (
       <ErrorBoundary>
-        <SwapiServiceProvider value = {this.swapiService}>
-        
-          <Header />
-          <RandomPlanet />
+        <SwapiServiceProvider value={this.swapiService}>
+          <Router>
+            <React.Fragment>
 
-          <PeoplePage />
-          <StarshipsPage />
-          <PlanetsPage />
-          
+              <Header />
+              <RandomPlanet />
+
+              <Route path='/'
+                     render={() => <h2>Welcome to Star DB!</h2>}
+                     exact={true}/>
+              <Route path='/people' component = {PeoplePage} />
+              <Route path='/planets' component={PlanetsPage} />
+              <Route path='/starships'
+                     component={StarshipsPage}
+                     exact={true}/>
+              <Route path='/starships/:id'
+                render={({ match }) => {
+                  
+                  const { id } = match.params;
+                  return <StarshipDetails itemId = {id} />
+                }}/>
+
+            </React.Fragment>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundary>
     );
   };
+
+  /* Компонент Router оборачивает те компоненты, внутри которых будет происходить роутинг.
+     Компонент Route определяет какие компоненты показывать при определенном url. */ 
 
 };
